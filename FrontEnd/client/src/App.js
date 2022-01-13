3e078264a537dea571648c7802555cc69f406d2d
Fonjihill <terence-hill.nzidjouo-fonji@epitech.eu>
2022-01-13 14:57:47 +0100

    first Commit

---
+++ FrontEnd/client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NotFound from "../src/404/notfound"
+
+function App() {
+  return (
+    <div className="App">
+     <NotFound/>
+    </div>
+  );
+}
+
+export default App;
